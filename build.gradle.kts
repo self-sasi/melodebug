42f1cb469da2e65fa60ad64f07e189075006a203
self-sasi <sarthaks281bond@gmail.com>
2026-02-27 19:51:35 -0700

    bundle opentelemetry java agent into meloagent for manual premain invocation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,14 @@ plugins {
     id("java")
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
+    // opentelemetry java agent bundled
+    implementation("io.opentelemetry.javaagent:opentelemetry-javaagent:2.25.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
