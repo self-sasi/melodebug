499ba4ec55da68b32ecbb20a204c30b6664e08c5
self-sasi <sarthaks281bond@gmail.com>
2026-03-15 20:22:29 -0600

    remove redundant junit import for root build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 tasks.test {
     useJUnitPlatform()
 }
